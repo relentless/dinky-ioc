f3724080a48ff670e6165708942fe8fb04580f73
grantac@hotmail.com <grantac@hotmail.com@a675ac1f-87b1-78b6-7f06-5d8a5918ff62>
2010-09-20 13:54:13 +0000

    Initial version.  Very limited functionality.

---
+++ Dinky.Tests/Container_Tests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Diggy;
+
+namespace Diggy.Tests {
+    
+    [TestFixture]
+    public class Container_Tests {
+        [Test]
+        public void DependencyMappedToExistingInstance_ReturnsExistingInstance() {
+            // arrange
+            Container container = new Container();
+            Dependency injectedDependency = new Dependency();
+
+            // act
+            container.map<IDependency>().to(injectedDependency);
+            IDependency result = container.resolve<IDependency>();
+
+            //assert
+            Assert.AreEqual(injectedDependency, result);
+        }
+
+        [Test]
+        public void DependencyMappedToNewInstance_CalledTwice_ReturnsTwoDifferentNewInstances() {
+            // arrange
+            Container container = new Container();
+
+            // act
+            container.map<IDependency>().toNew<Dependency>();
+            IDependency result1 = container.resolve<IDependency>();
+            IDependency result2 = container.resolve<IDependency>();
+
+            //assert
+            Assert.AreNotEqual(result1, result2);
+        }
+
+        [Test]
+        public void DependencyHasDependency_ReturnsDependencyWithDependencyResolved() {
+            // arrange
+            Container container = new Container();
+
+            // act
+            container.map<IDependant>().toNew<Dependant>();
+            IDependant result = container.resolve<IDependant>();
+
+            //assert
+            //Assert.AreEqual(injectedDependency, result);
+        }
+    }
+
+    public class Dependency : IDependency { }
+
+    public interface IDependant { }
+
+    public class Dependant: IDependant {
+        public Dependant(IDependency dependency) { }
+    }
+
+    public interface IDependency {}
+
+    //internal class Dependent {
+    //    internal IDependency _dependency;
+    //    internal Dependent(IDependency dependency) {
+    //        _dependency = dependency;
+    //    }
+    //}
+}
---
+++ Dinky/Container.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Diggy {
+    public class Container {
+
+        protected Dictionary<Type, Func<object>> _dependencies = new Dictionary<Type, Func<object>>();
+
+        public Container() {
+
+        }
+
+        public ContainerResolutionType map<T>() {
+            return new ContainerResolutionType(this, typeof(T));
+        }
+
+        internal void AddDepencency(Type Type, Func<object> Dependency) {
+            _dependencies.Add(Type, Dependency);
+        }
+
+        public T resolve<T>(){
+            Func<object> dependency = null;
+            if (_dependencies.TryGetValue(typeof(T), out dependency)) {
+                return (T)dependency.Invoke();
+            }
+            return default(T);
+        }
+    }
+
+    public class ContainerResolutionType {
+        private Container _container;
+        private Type _type;
+
+        internal ContainerResolutionType(Container Container, Type Type) {
+            _container = Container;
+            _type = Type;
+        }
+
+        public void to(object dependency) {
+            _container.AddDepencency(_type, delegate { return dependency; });
+        }
+
+        public void toNew<T>() {
+            _container.AddDepencency(_type, delegate { return Activator.CreateInstance<T>(); });
+        }
+    }
+}
